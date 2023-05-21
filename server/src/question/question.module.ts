0e9b2028265de6b5661c98250af50afbe77e5200
khkmgch <kohei.kamiguchi@gmail.com>
2023-05-21 16:27:02 +0900

    add: import PrismaModule to QuestionModule

--- server/src/question/question.module.ts
+++ server/src/question/question.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [QuestionController],
-  providers: [QuestionService]
+  providers: [QuestionService],
 })
 export class QuestionModule {}
