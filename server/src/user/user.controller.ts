5e97286ff98f38cf8cbc6445b92812602b7c123f
khkmgch <kohei.kamiguchi@gmail.com>
2023-05-21 16:04:26 +0900

    add: UserController

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,34 +1,74 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
+import { User_WithRelation } from 'src/types/prisma-extended/user-with-relation.type';
+import { IMsg } from 'src/auth/interfaces/i-msg.interface';
 
+//jwtによるプロテクションを'user'の全てのエンドポイントに適用
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
+  //ログインしているユーザーの情報を取得
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  getLoginUser(@Req() req: Request) {
+    //nestJsではjwt.strategy.ts内のvalidateメソッドの処理の際に、自動的にRequestの中にuserを含めてくれるので、
+    //controller内でRequestからユーザー情報にアクセスできる。
+    return req.user;
   }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  getUserById(
+    @Param('id', ParseIntPipe) userId: number,
+  ): Promise<Omit<User_WithRelation, 'hashedPassword'>> {
+    return this.userService.getUserById(userId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  //ユーザ情報を更新
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User_WithRelation, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
   }
 
+  //ユーザー削除
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  deleteUser(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) userId: number,
+  ): Promise<IMsg> {
+    return this.userService.deleteUser(req.user.id, userId);
+  }
+
+  //ユーザーをフォロー
+  @Patch(':id/follow')
+  followUser(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) userId: number,
+  ): Promise<IMsg> {
+    return this.userService.followUser(req.user.id, userId);
+  }
+
+  //ユーザーのフォローを外す
+  @Patch(':id/unfollow')
+  unfollowUser(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) userId: number,
+  ): Promise<IMsg> {
+    return this.userService.unfollowUser(req.user.id, userId);
   }
 }
