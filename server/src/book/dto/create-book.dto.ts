efef60d3743e5afc49f7924edbe8b732777751ac
khkmgch <kohei.kamiguchi@gmail.com>
2023-05-21 21:33:03 +0900

    add: dto

--- server/src/book/dto/create-book.dto.ts
+++ server/src/book/dto/create-book.dto.ts
@@ -1,1 +1,39 @@
-export class CreateBookDto {}
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  googleBooksId: string;
+
+  @IsString()
+  @IsOptional()
+  isbn?: string;
+
+  @IsArray()
+  @IsOptional()
+  authors?: string[];
+
+  @IsString()
+  @IsOptional()
+  publisher?: string;
+
+  @IsString()
+  @IsOptional()
+  publishedDate?: string;
+
+  @IsString()
+  @IsOptional()
+  pageCount?: string;
+
+  @IsString()
+  @IsOptional()
+  imgLink?: string;
+
+  @IsString()
+  @IsOptional()
+  previewLink?: string;
+}
