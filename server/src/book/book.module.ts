fd3f92c004d26fde772adbca901d751d666fe8d4
khkmgch <kohei.kamiguchi@gmail.com>
2023-05-21 21:32:52 +0900

    add: import PrismaModule, HttpModule to BookModule

--- server/src/book/book.module.ts
+++ server/src/book/book.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HttpModule],
   controllers: [BookController],
-  providers: [BookService]
+  providers: [BookService],
 })
 export class BookModule {}
