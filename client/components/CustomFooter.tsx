79cb45e1fd25e3a49941243029dd6b0e30019acc
khkmgch <kohei.kamiguchi@gmail.com>
2023-05-24 15:51:25 +0900

    add: components

---
+++ client/components/CustomFooter.tsx
@@ -0,0 +1,19 @@
+import { Footer } from '@mantine/core';
+import React from 'react';
+
+export const CustomFooter = () => {
+  return (
+    <Footer height={60} p='md' className='bg-slate-50'>
+      {/* <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <span>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
+      </a> */}
+    </Footer>
+  );
+};
