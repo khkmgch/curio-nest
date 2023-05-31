5913b2cbc378dd2e4db902f1102171a07fc8ab08
khkmgch <kohei.kamiguchi@gmail.com>
2023-05-31 16:16:25 +0900

    add: getBooksRecommendedByAI

--- server/src/book/book.controller.ts
+++ server/src/book/book.controller.ts
@@ -1,64 +1,93 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
-import { Book_WithRelation } from "src/types/prisma-extended/book-with-relation.type";
-import { BookService } from "./book.service";
-import { CreateBookDto } from "./dto/create-book.dto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { Book_WithRelation } from 'src/types/prisma-extended/book-with-relation.type';
+import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { GetChatgptResponseDto } from 'src/chatgpt/dto/get-chatgpt-response.dto';
 
-  @UseGuards(AuthGuard('jwt'))
-  @Controller('book')
-  export class BookController {
-    constructor(private readonly bookService: BookService) {}
-  
-    //ログインしているユーザーの本棚にある本を全て取得する
-    @Get('all/shelf')
-    getLoginBooks(
-      @Req() req: Request,
-    ): Promise<Book_WithRelation[]> {
-      return this.bookService.getBooks(req.user.id);
-    }
-  
-    //ユーザーの本棚にある本を全て取得する
-    @Get('all/shelf/:id')
-    getBooksById(
-      @Req() req: Request,
-      @Param('id', ParseIntPipe) userId: number,
-    ): Promise<Book_WithRelation[]> {
-      return this.bookService.getBooks(userId);
-    }
-  
-    //ログインしているユーザーの本棚から特定の本を１つ返す
-    @Get(':id')
-    getBookById(
-      @Req() req: Request,
-      @Param('id', ParseIntPipe) bookId: number,
-    ): Promise<Book_WithRelation> {
-      return this.bookService.getBookById(req.user.id, bookId);
-    }
-    //本をキーワード検索(google books api)
-    @Get('search/:keyword')
-    searchBooks(@Param('keyword') keyword: string) {
-      return this.bookService.searchBooks(keyword);
-    }
-  
-    //本を本棚に新規追加
-    @Post()
-    createBook(
-      @Req() req: Request,
-      @Body() dto: CreateBookDto,
-    ): Promise<Book_WithRelation> {
-      return this.bookService.createBook(req.user.id, dto);
-    }
-  
-    //本を本棚から削除
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Delete(':id')
-    deleteBookById(
-      @Req() req: Request,
-      @Param('id', ParseIntPipe) bookId: number,
-    ): Promise<void> {
-      return this.bookService.deleteBook(req.user.id, bookId);
-    }
+@UseGuards(AuthGuard('jwt'))
+@Controller('book')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  //ログインしているユーザーの本棚にある本を全て取得する
+  @Get('all/shelf')
+  getLoginBooks(
+    @Req() req: Request,
+  ): Promise<Book_WithRelation[]> {
+    return this.bookService.getBooks(req.user.id);
   }
-  
 
+  //ユーザーの本棚にある本を全て取得する
+  @Get('all/shelf/:id')
+  getBooksById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) userId: number,
+  ): Promise<Book_WithRelation[]> {
+    return this.bookService.getBooks(userId);
+  }
+
+  //ログインしているユーザーの本棚から特定の本を１つ返す
+  @Get(':id')
+  getBookById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) bookId: number,
+  ): Promise<Book_WithRelation> {
+    return this.bookService.getBookById(
+      req.user.id,
+      bookId,
+    );
+  }
+
+  @Post('recommend')
+  getBooksRecommendedByAI(
+    @Body() dto: GetChatgptResponseDto,
+  ) {
+    return this.bookService.getBooksRecommendedByAI(
+      dto.prompt,
+    );
+  }
+
+  @Get('recommend/test')
+  test() {
+    return this.bookService.test();
+  }
+
+  //本をキーワード検索(google books api)
+  @Get('search/:keyword')
+  searchBooks(@Param('keyword') keyword: string) {
+    return this.bookService.searchBooks(keyword);
+  }
+
+  //本を本棚に新規追加
+  @Post()
+  createBook(
+    @Req() req: Request,
+    @Body() dto: CreateBookDto,
+  ): Promise<Book_WithRelation> {
+    return this.bookService.createBook(req.user.id, dto);
+  }
+
+  //本を本棚から削除
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteBookById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) bookId: number,
+  ): Promise<void> {
+    return this.bookService.deleteBook(req.user.id, bookId);
+  }
+}
--- server/src/book/book.service.ts
+++ server/src/book/book.service.ts
@@ -1,17 +1,23 @@
-import { HttpService } from "@nestjs/axios";
-import { ForbiddenException, Injectable, Logger } from "@nestjs/common";
-import { catchError, firstValueFrom } from "rxjs";
-import { PrismaService } from "src/prisma/prisma.service";
-import { Book_WithRelation } from "src/types/prisma-extended/book-with-relation.type";
-import { CreateBookDto } from "./dto/create-book.dto";
-import { IGoogleBooks } from "./interfaces/i-google-books.interface";
-
+import { HttpService } from '@nestjs/axios';
+import {
+  ForbiddenException,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Book_WithRelation } from 'src/types/prisma-extended/book-with-relation.type';
+import { CreateBookDto } from './dto/create-book.dto';
+import { IGoogleBooks } from './interfaces/i-google-books.interface';
+import { ChatgptService } from 'src/chatgpt/chatgpt.service';
+import { NLPUtil } from './utils/nlp.util';
 
 @Injectable()
 export class BookService {
   private readonly logger = new Logger(BookService.name);
   constructor(
     private prisma: PrismaService,
+    private readonly chatgpt: ChatgptService,
     private readonly httpService: HttpService,
   ) {}
 
@@ -32,7 +38,10 @@ export class BookService {
   }
 
   //ログインしているユーザーの本棚から特定の本を１つ返す
-  getBookById(userId: number, bookId: number): Promise<Book_WithRelation> {
+  getBookById(
+    userId: number,
+    bookId: number,
+  ): Promise<Book_WithRelation> {
     return this.prisma.book.findFirst({
       where: {
         userId,
@@ -44,7 +53,9 @@ export class BookService {
     });
   }
   //本をキーワード検索(google books api)
-  async searchBooks(keyword: string): Promise<IGoogleBooks> {
+  async searchBooks(
+    keyword: string,
+  ): Promise<IGoogleBooks> {
     const { data } = await firstValueFrom(
       this.httpService
         .get<IGoogleBooks>(
@@ -59,6 +70,34 @@ export class BookService {
     );
     return data;
   }
+  async getBooksRecommendedByAI(prompt: string) {
+    const text = (await this.chatgpt.getResponse(prompt))
+      .content;
+    const titles: string[] =
+      await NLPUtil.extractBookTitles(text);
+    const books = [];
+    for (let i = 0; i < 3 && i < titles.length; i++) {
+      console.log(titles[i]);
+      books.push(await this.searchBooks(titles[i]));
+    }
+    console.log('books: ', books);
+    const res = [];
+    for (let i = 0; i < books.length; i++) {
+      res.push(books[i].items[0]);
+      res.push(books[i].items[1]);
+    }
+    return res;
+  }
+  async test() {
+    const text1 =
+      '1. 「なぜ空は青いのか？」2. 「光の色に騙されないために――「空の青」とは何か」3. 「色のなぞ――私たちの五感を支配する色の不思議」';
+    const text2 =
+      '1. 『自然現象を科学する』（著者: 佐藤健太郎）自然現象の中でも、空の色について詳しく解説しています。 2. 『空の色と光の不思議』（著者: 熊谷信）空の色や光の性質についての基礎知識を分かりやすく解説しています。3. 『天空の色彩学』（著者: 宇田川洋）';
+    const text3 =
+      '1. 自然現象を科学する（著者: 佐藤健太郎）自然現象の中でも、空の色について詳しく解説しています。 2. 空の色と光の不思議（著者: 熊谷信）空の色や光の性質についての基礎知識を分かりやすく解説しています。3. 天空の色彩学（著者: 宇田川洋）';
+
+    return NLPUtil.extractBookTitles(text3);
+  }
 
   //本を本棚に新規追加
   async createBook(
@@ -67,7 +106,10 @@ export class BookService {
   ): Promise<Book_WithRelation> {
     try {
       //googleBooksId, userIdでMy本棚から本を探し、本が存在する場合は新規追加をしない。
-      const found = await this.findByGoogleBooksId(dto.googleBooksId, userId);
+      const found = await this.findByGoogleBooksId(
+        dto.googleBooksId,
+        userId,
+      );
       if (found) {
         return null;
       }
@@ -102,14 +144,19 @@ export class BookService {
   }
 
   //本を本棚から削除する
-  async deleteBook(userId: number, bookId: number): Promise<void> {
+  async deleteBook(
+    userId: number,
+    bookId: number,
+  ): Promise<void> {
     const book = await this.prisma.book.findUnique({
       where: {
         id: bookId,
       },
     });
     if (!book || book.userId !== userId) {
-      throw new ForbiddenException('No permission to delete');
+      throw new ForbiddenException(
+        'No permission to delete',
+      );
     }
     await this.prisma.book.delete({
       where: {
@@ -118,5 +165,3 @@ export class BookService {
     });
   }
 }
-
-
