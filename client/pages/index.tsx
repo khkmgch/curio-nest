bdd75743a582f8ee1dc4e0db6e3cd7d8ce7644b8
khkmgch <kohei.kamiguchi@gmail.com>
2023-06-04 22:36:53 +0900

    update: Home

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -10,36 +10,23 @@ import { Layout } from '../components/Layout';
 const Home: NextPage = () => {
   return (
     <Layout title='Home'>
-      <div className='h-screen w-screen'>
-        <div className='mb-10 flex h-1/2 w-full justify-center'>
-          <div className='w-192'>
-            <div className='flex justify-center'>
-              <IconQuestionMark size={150} />
-            </div>
-            <div className='mx-10 mt-0 flex justify-around'>
-              <IconCornerRightDown
-                size={90}
-                className='rotate-270 '
-              />
-              <IconCornerRightDown size={90} className='' />
-            </div>
-
-            <div className='mt-10 flex justify-around'>
-              <IconBulb size={150} />
-              <IconBook size={150} />
-            </div>
-            <div className='flex justify-center'>
-              <IconCornerRightDown
-                size={90}
-                className='rotate-135'
-              />
-            </div>
-          </div>
+      <div className='relative h-screen w-screen'>
+        <div className='absolute left-24 top-8 text-10xl font-bold text-dark-cyan-100/50'>
+          <div>Curiosity, Book</div>
+          <div>& Inspiration</div>
         </div>
-        <div className='bg-custom-blue-0 flex h-1/3 w-full items-center justify-center'>
-          <p className='text-center text-2xl font-semibold'>
-            日々の疑問を簡単に管理
+        <div className='flex h-1/3 w-full items-center justify-center'>
+          <p className='absolute left-48 top-1/2 text-5xl text-bright-blue-500 underline decoration-bright-yellow-500 decoration-4'>
+            Let's manage your daily curiosity.
           </p>
+        </div>
+
+        <div className='absolute left-2/3 top-1/4 w-1/3 '>
+          <img
+            src='/home-logo.png'
+            alt='logo'
+            className='object-contain'
+          />
         </div>
       </div>
     </Layout>
